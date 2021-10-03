d393d597375ca289d1455dd706f7e840916f43ec
Joseph Cileo <joseph.cileo@gmail.com>
2021-10-03 22:20:32 +0200

    testing

---
+++ Detection.cpp
@@ -0,0 +1,5 @@
+//
+// Created by jc on 10/3/21.
+//
+
+#include "Detection.h"
---
+++ Detection.h
@@ -0,0 +1,14 @@
+//
+// Created by jc on 10/3/21.
+//
+
+#ifndef OPENCVTESTING_DETECTION_H
+#define OPENCVTESTING_DETECTION_H
+
+
+class Detection {
+
+};
+
+
+#endif //OPENCVTESTING_DETECTION_H
